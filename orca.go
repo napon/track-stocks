ab2a1c3745ac12aee2ab49beb9081a8a9197b532
Napon Taratan <napon@napontaratan.com>
2016-08-07 16:39:19 -0700

    Added support for Thai stocks and fixed timezone error

--- orca.go
+++ orca.go
@@ -2,11 +2,10 @@
 // Usage: go run orca.go [csv_file]
 //			 [update_time]
 //
-// update_time := format HH:mm
-// csv_file := [symbol,price,amount]
+// update_time := format HH:mm UTC time
+// csv_file := [symbol,price,amount,isThaiStock]
 // see sample_input.csv for more info
 
-// TODO: Support Thailand stocks
 // TODO: Support ErrorMessage handling from API call
 
 package main
@@ -27,18 +26,23 @@ import (
 )
 
 const MAILGUN_URL = "___URL___"
-const MAILGUN_PRIVATE_KEY = "___PRIVATE_API_KEY___"
-const MAILGUN_PUBLIC_KEY = "___PUBLIC_API_KEY___"
+const MAILGUN_PRIVATE_KEY = "___API_PRIVATE_KEY___"
+const MAILGUN_PUBLIC_KEY = "___API_PUBLIC_KEY___"
 const USER_EMAIL = "___EMAIL___"
-const QUOTE_BASE_URL = "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol="
-const DEBUG = false
 
-var user_stocks []UserStock
+const QUOTE_BASE_URL = "https://finance.yahoo.com/webservice/v1/symbols/"
+const QUOTE_TH_STOCK = ".BK"
+const QUOTE_URL_SUFFIX = "/quote?format=json"
+const QUOTE_USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0.1; MotoG3 Build/MPI24.107-55) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.81 Mobile Safari/537.36"
+
+const DEBUG = true
+
+var user_stocks map[string]*UserStock
 var update_time string
 
 type UserStock struct {
-	Symbol string
-	Shares []Share
+	Shares []*Share
+	IsThai bool
 }
 
 type Share struct {
@@ -46,23 +50,13 @@ type Share struct {
 	Amount int
 }
 
+type Response struct {
+	Quotes []*Quote
+}
+
 type Quote struct {
-	Status           string
-	Name             string
-	Symbol           string
-	LastPrice        float64
-	Change           float64
-	ChangePercent    float64
-	Timestamp        string
-	MSDate           float64
-	MarketCap        float64
-	Volume           float64
-	ChangeYTD        float64
-	ChangePercentYTD float64
-	High             float64
-	Low              float64
-	Open             float64
-	Message          string // Error message
+	Symbol    string
+	LastPrice float64
 }
 
 // DEBUG: Print message to console.
@@ -80,6 +74,26 @@ func checkError(e error) {
 	}
 }
 
+// Override: flatten structure of response into Quote.
+func (response *Response) UnmarshalJSON(b []byte) error {
+	var f interface{}
+	json.Unmarshal(b, &f)
+	m := f.(map[string]interface{})
+	n := m["list"].(map[string]interface{})
+	v := n["resources"]
+	resources := v.([]interface{})
+
+	for _, resource := range resources {
+		r := resource.(map[string]interface{})["resource"].(map[string]interface{})
+		stock := r["fields"].(map[string]interface{})
+		symbol := strings.ToUpper(stock["symbol"].(string))
+		price, _ := strconv.ParseFloat(stock["price"].(string), 64)
+		response.Quotes = append(response.Quotes, &Quote{symbol, price})
+	}
+
+	return nil
+}
+
 // Parse input data into user_stocks.
 func populateUserStocks(csv_file string) {
 	debugMessage("populateUserStocks csv: ", csv_file)
@@ -91,57 +105,40 @@ func populateUserStocks(csv_file string) {
 	records, err := r.ReadAll()
 	checkError(err)
 
-	debugMessage(records)
+	debugMessage("records:", records)
 
-	i := 1
-	var currentSymbol string
-	var currentShares []Share
-	for i < len(records) {
+	for i := 1; i < len(records); i++ {
 		record := records[i]
-		if i == 1 {
-			currentSymbol = record[0]
-		}
+		symbol := strings.ToUpper(record[0])
+		price, err := strconv.ParseFloat(record[1], 64)
+		checkError(err)
+		amount, err := strconv.Atoi(record[2])
+		checkError(err)
+		isBKKStock, err := strconv.ParseBool(record[3])
+		checkError(err)
 
-		if currentSymbol == record[0] {
-			price, err := strconv.ParseFloat(record[1], 64)
-			checkError(err)
-			amount, err := strconv.Atoi(record[2])
-			checkError(err)
-			currentShares = append(currentShares, Share{price, amount})
-			i += 1
+		stock := user_stocks[symbol]
+		share := &Share{price, amount}
+
+		if stock == nil {
+			stock = &UserStock{IsThai: isBKKStock, Shares: []*Share{share}}
+			user_stocks[symbol] = stock
 		} else {
-			user_stocks = append(user_stocks, UserStock{currentSymbol, currentShares})
-
-			// Clear data for a new stock.
-			currentSymbol = record[0]
-			currentShares = make([]Share, 0)
+			stock.Shares = append(stock.Shares, share)
 		}
 	}
 
-	// Add last stock item.
-	user_stocks = append(user_stocks, UserStock{currentSymbol, currentShares})
-	debugMessage(user_stocks)
-}
-
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Printf("Usage: ./orca [csv_file] [update_time]\n")
-		os.Exit(1)
-	}
-
-	user_stocks = make([]UserStock, 0)
-	populateUserStocks(os.Args[1])
-	update_time = os.Args[2]
-
-	gocron.Clear()
-	gocron.Every(1).Day().At(update_time).Do(fetchAndSendUpdate)
-	<-gocron.Start()
+	debugMessage("user_stocks:", user_stocks)
 }
 
 // Fetch updated stocks and send email.
 func fetchAndSendUpdate() {
 	update := getUserStockPriceResult()
-	debugMessage(update)
+	debugMessage("\n\nOutput result:", update)
+
+	if DEBUG {
+		return
+	}
 
 	mg := mailgun.NewMailgun(
 		MAILGUN_URL,
@@ -150,7 +147,7 @@ func fetchAndSendUpdate() {
 	)
 
 	email := mg.NewMessage(
-		"ORCA PROJECT <mailgun@"+MAILGUN_URL+">",
+		"ORCA PROJECT <orca@"+MAILGUN_URL+">",
 		"ORCA Daily Update",
 		update,
 		USER_EMAIL,
@@ -158,24 +155,28 @@ func fetchAndSendUpdate() {
 
 	_, _, err := mg.Send(email)
 	checkError(err)
+
+	fmt.Println("[", update_time, "] Sent an update to", USER_EMAIL)
 }
 
 // Return a well formatted result of current market prices.
 func getUserStockPriceResult() string {
 	investment := 0.0
 	returnInvestment := 0.0
-	result := "Stocks Update " + time.Now().Format("Tue Jan 12") + "\n"
+	result := "Stocks Update - " + time.Now().Format("Mon Jan 2") + "\n"
 	result += "-----------------------------\n"
 	result += fmt.Sprintf("%-*s %-*s\n", 10, "Symbol", 15, "M.Price")
-	for _, stock := range user_stocks {
-		quote := fetchQuoteForStock(stock.Symbol)
-		price := quote.LastPrice
-		result += fmt.Sprintf("%-*s %-*.2f\n", 10, strings.ToUpper(stock.Symbol), 15, price)
+
+	quotes := fetchStocksUpdate()
+	for symbol, stock := range user_stocks {
+		price := quotes[symbol].LastPrice
+		result += fmt.Sprintf("%-*s %-*.2f\n", 10, symbol, 15, price)
 		for _, share := range stock.Shares {
 			investment += share.Price * float64(share.Amount)
 			returnInvestment += price * float64(share.Amount)
 		}
 	}
+
 	result += "-----------------------------\n"
 	result += "Total amount invested: " + fmt.Sprintf("%.2f\n", investment)
 	result += "Current amount: " + fmt.Sprintf("%.2f\n", returnInvestment)
@@ -183,24 +184,79 @@ func getUserStockPriceResult() string {
 	return result
 }
 
-// Return market price of a stock.
-func fetchQuoteForStock(code string) *Quote {
-	url := QUOTE_BASE_URL + strings.ToLower(code)
-	httpResponse := getHttpResponse(url)
+// Return market prices of user's stocks.
+func fetchStocksUpdate() map[string]*Quote {
+	var symbols []string
+	for k, v := range user_stocks {
+		s := k
+		if v.IsThai {
+			s += QUOTE_TH_STOCK
+		}
+		symbols = append(symbols, s)
+	}
+	debugMessage("stocks to fetch:", symbols)
+	return fetchQuoteForStocks(strings.Join(symbols, ","))
+}
 
-	var quote Quote
-	err := json.Unmarshal(httpResponse, &quote)
+// Return market price of stocks.
+// Args: codes - list of comma separated symbols (eg. AAPL,MSFT)
+func fetchQuoteForStocks(codes string) map[string]*Quote {
+	url := QUOTE_BASE_URL + codes + QUOTE_URL_SUFFIX
+	debugMessage("url:", url)
+
+	httpResponse := getHttpResponse(url)
+	debugMessage("http response:", string(httpResponse))
+
+	var r Response
+	err := json.Unmarshal(httpResponse, &r)
 	checkError(err)
-	return &quote
+
+	quotes := make(map[string]*Quote)
+	for _, q := range r.Quotes {
+		// Remove country specific annotation.
+		symbol := strings.Split(q.Symbol, ".")[0]
+		quotes[symbol] = q
+	}
+
+	return quotes
 }
 
 // Return byte array response of an API call.
 func getHttpResponse(url string) []byte {
-	resp, err := http.Get(url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	checkError(err)
+
+	req.Header.Set("User-Agent", QUOTE_USER_AGENT)
+	resp, err := client.Do(req)
 	checkError(err)
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	checkError(err)
 	return body
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: ./orca [csv_file] [update_time]\n")
+		os.Exit(1)
+	}
+
+	fmt.Println("[", time.Now().UTC().Format("15:04"), "] Started running..")
+
+	user_stocks = make(map[string]*UserStock)
+	populateUserStocks(os.Args[1])
+	update_time = os.Args[2]
+
+	gocron.Remove(fetchAndSendUpdate)
+	gocron.Clear()
+
+	if DEBUG {
+		fetchAndSendUpdate()
+	} else {
+		gocron.ChangeLoc(time.UTC)
+		gocron.Every(1).Day().At(update_time).Do(fetchAndSendUpdate)
+		<-gocron.Start()
+	}
 }
